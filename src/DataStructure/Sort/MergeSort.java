9f8e5d916e13112c80e13828e5704cef503eb016
995689575@qq.com <995689575@qq.com>
2019-06-12 18:08:55 +0800

    排序算法 :归并排序

--- src/DataStructure/Sort/MergeSort.java
+++ src/DataStructure/Sort/MergeSort.java
@@ -50,7 +50,6 @@ public class MergeSort {
         // left - mid   mid+1 - right 两个序列已经排好序，但整体无序
         // merge的过程就是将两个序列排序
         merge(arr, left, mid, right);
-//        mergedayin(arr, left, mid, right);
     }
 
     private static void merge(int[] arr, int left, int mid, int right) {
@@ -72,35 +71,11 @@ public class MergeSort {
         }
         //并不是每次都拷贝所有
         //两个两个的拷贝
+//        System.out.println("left---->" + left);
+//        System.out.println("right---->" + right);
         for (i = 0; i < help.length; i++) {
             arr[left + i] = help[i];
         }
-    }
-
-    private static void mergedayin(int[] arr, int left, int mid, int right) {
-        // 申请辅助数组,存放合并后的序列
-        int[] help = new int[right - left + 1];
-        int i = 0;//指向 help 数组的当前索引
-        int p1 = left;
-        int p2 = mid + 1;
-        while (p1 <= mid && p2 <= right) {
-            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
-        }
-        // 上面 while 两个必有且只有一个 数组先遍历完毕
-        // 两个循环只会执行一个
-        while (p1 <= mid) {
-            help[i++] = arr[p1++];
-        }
-        while (p2 <= right) {
-            help[i++] = arr[p2++];
-        }
-        //并不是每次都拷贝所有
-        //两个两个的拷贝
-        System.out.println("left---->" + left);
-        System.out.println("right---->" + right);
-        for (i = 0; i < help.length; i++) {
-            arr[left + i] = help[i];
-        }
-        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(arr));
     }
 }
