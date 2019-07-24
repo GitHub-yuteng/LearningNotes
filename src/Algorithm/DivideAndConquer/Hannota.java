67f25afeebd945b108980c68f6545dccd703ca40
995689575@qq.com <995689575@qq.com>
2019-07-24 16:16:01 +0800

    汉诺塔

--- src/Algorithm/DivideAndConquer/Hannota.java
+++ src/Algorithm/DivideAndConquer/Hannota.java
@@ -6,14 +6,16 @@ package Algorithm.DivideAndConquer;
 public class Hannota {
 
     public static void main(String[] args) {
-        Hannota(10, 'A', 'B', 'C');
+        Hannota(5, 'A', 'B', 'C');
     }
 
     public static void Hannota(int num, char a, char b, char c) {
+
         //如果只有一个盘
         if (num == 1) {
             System.out.println("第1个盘从 " + a + "->" + c);
         } else {
+
             //如果n>=2,总看成两个盘子，上面 和 下面
             //1、先把上面的所有的盘 A->B,移动过程用到 C
             //上面
