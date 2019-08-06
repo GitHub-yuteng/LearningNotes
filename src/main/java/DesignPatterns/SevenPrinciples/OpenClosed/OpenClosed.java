cf397df06046be1c903219066e70c9507a125c5a
yuteng <soft_yt@163.com>
2019-08-06 09:49:15 +0800

    设计模式-开闭原则

--- src/main/java/DesignPatterns/SevenPrinciples/OpenClosed/OpenClosed.java
+++ src/main/java/DesignPatterns/SevenPrinciples/OpenClosed/OpenClosed.java
@@ -1,4 +1,4 @@
-package DesignPatterns.Principle.OpenClosed;
+package DesignPatterns.SevenPrinciples.OpenClosed;
 
 /**
  * @author Yu
@@ -6,7 +6,7 @@ package DesignPatterns.Principle.OpenClosed;
  * <p>
  * 1、开闭原则(Open Closed Principle) 是编程中最基础、最重要的设计原则
  * 2、一个软件实体，比如类，模块和函数应该对提供方扩展开放，对使用方修改关闭。用抽象构建框架，用实现扩展细节。
- * 3、当软件需要变化时，尽量通过扩展软件实体的行为来实现变化，而不是通过修改已有的代码来实现变化。
+ * 3、//TODO 当软件需要变化时，尽量通过扩展软件实体的行为来实现变化，而不是通过修改已有的代码来实现变化。
  * 4、编程中遵循其它原则，以及使用设计模式的目的就是遵循开闭原则。
  */
 public class OpenClosed {
