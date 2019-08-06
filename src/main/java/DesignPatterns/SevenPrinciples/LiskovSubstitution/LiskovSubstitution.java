392910e9485570bcec803f520a8e1a568eb87d6e
yuteng <soft_yt@163.com>
2019-08-06 09:48:59 +0800

    设计模式-里氏替换原则(尽量解耦，使用依赖、聚合、组合方式)

--- src/main/java/DesignPatterns/SevenPrinciples/LiskovSubstitution/LiskovSubstitution.java
+++ src/main/java/DesignPatterns/SevenPrinciples/LiskovSubstitution/LiskovSubstitution.java
@@ -1,4 +1,4 @@
-package DesignPatterns.Principle.LiskovSubstitution;
+package DesignPatterns.SevenPrinciples.LiskovSubstitution;
 
 /**
  * @author Yu
@@ -7,11 +7,11 @@ package DesignPatterns.Principle.LiskovSubstitution;
  * 1、里氏替换原则(Liskov Substitution Principle)在1988年，由麻省理工学院的以为姓里的女士提出的。
  * <p>
  * 2、如果对每个类型为T1的对象o1，都有类型为T2的对象o2，使得以T1定义的所有程序
- * P在所有的对象o1都代换成o2时，程序P的行为没有发生变化，那么类型T2是类型T1
- * 的子类型。换句话说，所有引用基类的地方必须能透明地使用其子类的对象。
- * <p>
+ *     P在所有的对象o1都代换成o2时，程序P的行为没有发生变化，那么类型T2是类型T1
+ *    的子类型。换句话说，所有引用基类的地方必须能透明地使用其子类的对象。
+ *
  * 3、在使用继承时，遵循里氏替换原则，在子类中尽量不要重写父类的方法；
- * 4、里氏替换原则告诉我们，继承实际上让两个类耦合性增强了，在适当的情况下，可以通过聚合，组合，依赖来解决问题。
+ * 4、//TODO 里氏替换原则告诉我们，继承实际上让两个类耦合性增强了，在适当的情况下，可以通过聚合，组合，依赖来解决问题。
  * 5、继承会给程序带来侵入性
  */
 public class LiskovSubstitution {
