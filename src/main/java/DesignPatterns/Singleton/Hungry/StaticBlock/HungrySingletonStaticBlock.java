75c6b6932d1b1d321f6798154e1da68205ed1bbf
yuteng <soft_yt@163.com>
2019-08-06 18:49:11 +0800

    单例模式-饿汉静态代码块

---
+++ src/main/java/DesignPatterns/Singleton/Hungry/StaticBlock/HungrySingletonStaticBlock.java
@@ -0,0 +1,31 @@
+package DesignPatterns.Singleton.Hungry.StaticBlock;
+
+/**
+ * @author Yu
+ */
+public class HungrySingletonStaticBlock {
+    public static void main(String[] args) {
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance1 == instance2);//true
+
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
+    }
+}
+
+class Singleton {
+
+    private static Singleton instance;
+
+    static {
+        instance = new Singleton();
+    }
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
