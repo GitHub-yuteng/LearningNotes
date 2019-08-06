28f3bed003226758b9a25a844c37784bf0284b6e
yuteng <soft_yt@163.com>
2019-08-06 09:47:34 +0800

    设计模式-依赖倒转原则

--- src/main/java/DesignPatterns/SevenPrinciples/DependenceInversion/DependenceInversion.java
+++ src/main/java/DesignPatterns/SevenPrinciples/DependenceInversion/DependenceInversion.java
@@ -1,4 +1,4 @@
-package DesignPatterns.Principle.DependenceInversion;
+package DesignPatterns.SevenPrinciples.DependenceInversion;
 
 /**
  * @author Yu
@@ -9,15 +9,15 @@ package DesignPatterns.Principle.DependenceInversion;
  * 3、依赖倒转(倒置)的中心思想是面向接口编程
  * <p>
  * 4、依赖倒转原则是基于这样的设计理念:相对于细节的多变性，抽象的东西要稳定的
- * 多。以抽象为基础搭建的架构比以细节为基础的架构要稳定的多。在java中， 抽象
- * 指的是接口或抽象类，细节就是具体的实现类
+ *    多。以抽象为基础搭建的架构比以细节为基础的架构要稳定的多。在java中， 抽象
+ *    指的是接口或抽象类，细节就是具体的实现类
  * <p>
  * 5、使用接口或抽象类的目的是制定好规范，而不涉及任何具体的操作，把展现细节的任务交给他们的实现类去完成
  *
  * 依赖关系三种传递方式：
- * 接口传递
- * 构造方法传递
- * setter方式传递
+ *  接口传递
+ *  构造方法传递
+ *  setter方式传递
  *
  */
 public class DependenceInversion {
