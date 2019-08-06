7e62f8aa69247f1166744d1a5183e7695b5d08fe
yuteng <soft_yt@163.com>
2019-08-06 18:49:22 +0800

    单例模式-饿汉静态常量

---
+++ src/main/java/DesignPatterns/Singleton/Hungry/StaticInstance/HungrySingletonStaticInstance.java
@@ -0,0 +1,31 @@
+package DesignPatterns.Singleton.Hungry.StaticInstance;
+
+/**
+ * @author Yu
+ */
+//TODO  饿汉式 —— 静态常量
+public class HungrySingletonStaticInstance {
+    public static void main(String[] args) {
+        Singleton instance1 = Singleton.getInstance();
+        Singleton instance2 = Singleton.getInstance();
+        System.out.println(instance1 == instance2);//true
+
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
+    }
+}
+
+class Singleton {
+
+    // 将自身实例化对象设置为一个属性，并用static、final修饰
+    private final static Singleton instance = new Singleton();
+
+    // 构造方法私有化
+    private Singleton() {
+    }
+
+    // 提供静态方法返回该实例
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
