80ebd895d92dff5551bb6a612a87329328e4eb25
yuteng <soft_yt@163.com>
2019-08-07 09:50:51 +0800

    单例模式-枚举

---
+++ src/main/java/DesignPatterns/Singleton/Enum/EnumSingleton.java
@@ -0,0 +1,24 @@
+package DesignPatterns.Singleton.Enum;
+
+public class EnumSingleton {
+    public static void main(String[] args) {
+        System.out.println("======EnumSingleton======");
+        Singleton instance1 = Singleton.INSTANCE;
+        Singleton instance2 = Singleton.INSTANCE;
+        System.out.println("instance1 == instance2：" + (instance1 == instance2));//true
+
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
+
+        instance1.method();
+        instance2.method();
+    }
+}
+
+enum Singleton {
+    INSTANCE;
+
+    public void method() {
+        System.out.println("EnumSingleton");
+    }
+}
