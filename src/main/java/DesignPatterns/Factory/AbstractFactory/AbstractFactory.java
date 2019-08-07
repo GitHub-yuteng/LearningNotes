316537ec47e02e7372a82826ea2ad7d03c8eb298
yuteng <soft_yt@163.com>
2019-08-07 17:35:22 +0800

    工厂模式-抽象工厂

--- src/main/java/DesignPatterns/Factory/AbstractFactory/AbstractFactory.java
+++ src/main/java/DesignPatterns/Factory/AbstractFactory/AbstractFactory.java
@@ -3,6 +3,7 @@ package DesignPatterns.Factory.AbstractFactory;
 import DesignPatterns.Factory.AbstractFactory.CPU.CPU;
 import DesignPatterns.Factory.AbstractFactory.Factory.Iphone8Factory;
 import DesignPatterns.Factory.AbstractFactory.Factory.IphoneXsFactory;
+import DesignPatterns.Factory.AbstractFactory.Factory.PhoneFactory;
 import DesignPatterns.Factory.AbstractFactory.Screen.Screen;
 
 import java.util.Calendar;
@@ -13,8 +14,8 @@ import java.util.Calendar;
 public class AbstractFactory {
     public static void main(String[] args) {
 
-        Iphone8Factory iphone8Factory = new Iphone8Factory();
-        IphoneXsFactory iphoneXSFactory = new IphoneXsFactory();
+        PhoneFactory iphone8Factory = new Iphone8Factory();
+        PhoneFactory iphoneXSFactory = new IphoneXsFactory();
 
         System.out.println("======iphone8Factory=====");
         CPU cpuA11 = iphone8Factory.getCpu();
