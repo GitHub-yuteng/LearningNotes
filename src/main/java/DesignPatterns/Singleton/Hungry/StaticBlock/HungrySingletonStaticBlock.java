d5677b04ed51538c387d84fd55dbab0c043d5e4c
yuteng <soft_yt@163.com>
2019-08-07 09:36:19 +0800

    单例模式-饿汉式 静态代码块

--- src/main/java/DesignPatterns/Singleton/Hungry/StaticBlock/HungrySingletonStaticBlock.java
+++ src/main/java/DesignPatterns/Singleton/Hungry/StaticBlock/HungrySingletonStaticBlock.java
@@ -5,9 +5,10 @@ package DesignPatterns.Singleton.Hungry.StaticBlock;
  */
 public class HungrySingletonStaticBlock {
     public static void main(String[] args) {
+        System.out.println("======HungrySingletonStaticBlock======");
         Singleton instance1 = Singleton.getInstance();
         Singleton instance2 = Singleton.getInstance();
-        System.out.println(instance1 == instance2);//true
+        System.out.println("instance1 == instance2：" + (instance1 == instance2));//true
 
         System.out.println(instance1.hashCode());
         System.out.println(instance2.hashCode());
