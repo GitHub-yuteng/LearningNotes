d751b72982e0a08a272015185fedc936f9e8dcc5
yuteng <soft_yt@163.com>
2019-08-14 17:16:14 +0800

    AOP 切面实现

---
+++ src/main/java/Spring/AOP/IOCTest_AOP.java
@@ -0,0 +1,19 @@
+package Spring.AOP;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class IOCTest_AOP {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MainConfigOfAOP.class);
+
+        //1、不要自己创建对象
+//		MathCalculator mathCalculator = new MathCalculator();
+//		mathCalculator.div(1, 1);
+        MathCalculator mathCalculator = applicationContext.getBean(MathCalculator.class);
+
+        mathCalculator.div(1, 1);
+
+        applicationContext.close();
+    }
+}
---
+++ src/main/java/Spring/AOP/LogAspects.java
@@ -0,0 +1,43 @@
+package Spring.AOP;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+
+import java.util.Arrays;
+
+/**
+ * @Aspect： 告诉Spring当前类是一个切面类
+ */
+@Aspect
+public class LogAspects {
+
+    //抽取公共的切入点表达式
+    //1、本类引用
+    //2、其他的切面引用
+    @Pointcut("execution(public int Spring.AOP.MathCalculator.*(..))")
+    public void pointCut() {
+    }
+
+    //@Before在目标方法之前切入；切入点表达式（指定在哪个方法切入）
+    @Before("pointCut()")
+    public void logStart(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        System.out.println("" + joinPoint.getSignature().getName() + "运行。。。@Before:参数列表是：{" + Arrays.asList(args) + "}");
+    }
+
+    @After("Spring.AOP.LogAspects.pointCut()")
+    public void logEnd(JoinPoint joinPoint) {
+        System.out.println("" + joinPoint.getSignature().getName() + "结束。。。@After");
+    }
+
+    //JoinPoint一定要出现在参数表的第一位
+    @AfterReturning(value = "pointCut()", returning = "result")
+    public void logReturn(JoinPoint joinPoint, Object result) {
+        System.out.println("" + joinPoint.getSignature().getName() + "正常返回。。。@AfterReturning:运行结果：{" + result + "}");
+    }
+
+    @AfterThrowing(value = "pointCut()", throwing = "exception")
+    public void logException(JoinPoint joinPoint, Exception exception) {
+        System.out.println("" + joinPoint.getSignature().getName() + "异常。。。异常信息：{" + exception + "}");
+    }
+}
