49589f388d09304623d972ec6cca6336a1c72597
yuteng <soft_yt@163.com>
2019-08-20 18:18:48 +0800

    适配器模式，对象适配器

--- src/main/java/DesignPatterns/StructuralType/Adapter/ObjectAdapter/VoltageAdapter.java
+++ src/main/java/DesignPatterns/StructuralType/Adapter/ObjectAdapter/VoltageAdapter.java
@@ -11,13 +11,13 @@ public class VoltageAdapter implements IVoltage5V {
 
     @Override
     public int output5V() {
-        int dst = 0;
+        int dstV = 0;
         if (null != voltage220V) {
             System.out.println("使用对象进行适配！");
-            int src = voltage220V.output220V();
-            dst = src / 44;
-            System.out.println("适配完成电压为：" + dst + "V");
+            int srcV = voltage220V.output220V();
+            dstV = srcV / 44;
+            System.out.println("适配完成电压为：" + dstV + "V");
         }
-        return dst;
+        return dstV;
     }
 }
