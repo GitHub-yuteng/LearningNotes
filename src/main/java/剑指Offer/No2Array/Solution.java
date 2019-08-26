b73c8bcb2bafa0940737b2c3dacdd7c47d9c4d61
yuteng <soft_yt@163.com>
2019-08-26 17:53:50 +0800

    给定一个二维数组，其每一行从左到右递增排序，从上到下也是递增排序。给定一个数，判断这个数是否在该二维数组中

--- src/main/java/剑指Offer/No2Array/Solution.java
+++ src/main/java/剑指Offer/No2Array/Solution.java
@@ -1,15 +1,14 @@
-package 剑指Offer.Interview_4;
+package 剑指Offer.No2Array;
 
-/*
-给定一个二维数组，其每一行从左到右递增排序，从上到下也是递增排序。给定一个数，判断这个数是否在该二维数组中。
-*/
-
-public class No2Array {
+/**
+ * 给定一个二维数组，其每一行从左到右递增排序，从上到下也是递增排序。给定一个数，判断这个数是否在该二维数组中。
+ */
+public class Solution {
     public static void main(String[] args) {
         int[][] array = {{1, 2, 8, 9},
-                        {2, 4, 9, 12},
-                        {4, 7, 10, 13},
-                        {6, 8, 11, 15}};
+                         {2, 4, 9, 12},
+                         {4, 7, 10, 13},
+                         {6, 8, 11, 15}};
         System.out.println(findByTheUpperRightCorner(10, array));
         System.out.println(findByTheLowerLeftCorner(7, array));
     }
@@ -23,14 +22,15 @@ public class No2Array {
         if (array.length > 0) {
             int rows = array.length;
             int columns = array[0].length;
+
             int row = 0;
             int column = columns - 1;
             while (row < rows && column >= 0) {
                 if (array[row][column] == target)
                     return true;
-                if (array[row][column] > target)
+                if (array[row][column] > target)// TODO 如果该数字大于要查找的数字，则剔除该数字所在的列；
                     column--;
-                else
+                else //TODO 如果该数字小于要查找的数字，则剔除该数字所在的行；
                     row++;
             }
             return false;
@@ -64,22 +64,3 @@ public class No2Array {
         return false;
     }
 }
-
-//剑指Offer
-/*
-public boolean Find(int target, int[][] matrix) {
-    if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
-        return false;
-    int rows = matrix.length, cols = matrix[0].length;
-    int r = 0, c = cols - 1; // 从右上角开始
-    while (r <= rows - 1 && c >= 0) {
-        if (target == matrix[r][c])
-            return true;
-        else if (target > matrix[r][c])
-            r++;
-        else
-            c--;
-    }
-    return false;
-}
-*/
