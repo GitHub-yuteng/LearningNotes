36c3e46c041e1c85fdda6621c5d71df1493ee8ad
yuteng <soft_yt@163.com>
2019-08-26 17:36:32 +0800

    将一个字符串中的空格替换成 "%20"

---
+++ src/main/java/剑指Offer/ReplaceSpace/Solution.java
@@ -0,0 +1,43 @@
+package 剑指Offer.ReplaceSpace;
+
+/**
+ * 将一个字符串中的空格替换成 "%20"。
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+        StringBuffer str = new StringBuffer("We Are Happy");
+        System.out.println(str.toString());
+        String replaceSpace = replaceSpace(str);
+        System.out.println(replaceSpace);
+    }
+
+    public static String replaceSpace(StringBuffer str) {
+        int length = str.length();//TODO 字符串长度
+        int count = 0;//TODO 空格个数
+
+        for (int i = 0; i < length; i++) {
+            if (str.charAt(i) == ' ') {
+                count++;
+            }
+        }
+
+
+        int len = 2 * count + length;//TODO 空格原本占一个位置，则空格数 乘 2 得总长
+        char[] chars = new char[len];//TODO 提前设置好数组大小，提高扩容效率
+        int index = len - 1;//TODO 因为数组下标从0开始
+
+        while (length > 0) {//TODO > 0 防止 下标越界
+            if (str.charAt(length - 1) != ' ') {
+                chars[index--] = str.charAt(length - 1);
+            } else {
+                chars[index--] = '0';
+                chars[index--] = '2';
+                chars[index--] = '%';
+            }
+            length--;
+        }
+        return String.valueOf(chars);
+    }
+}
