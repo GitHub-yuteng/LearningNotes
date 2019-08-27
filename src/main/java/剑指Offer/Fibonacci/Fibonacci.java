b59f759e5cb2a438f210af5709b0019c03f463c0
yuteng <soft_yt@163.com>
2019-08-27 15:46:24 +0800

    Fibonacci 递归

--- src/main/java/剑指Offer/Fibonacci/Fibonacci.java
+++ src/main/java/剑指Offer/Fibonacci/Fibonacci.java
@@ -6,14 +6,17 @@ package 剑指Offer.Fibonacci;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println(fibonacci(3));
+        System.out.println(fibonacci(30));
     }
 
-    private static int fibonacci(int i) {
-        if (i == 1 || i == 2) {
+    private static int fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
             return 1;
         } else {
-            return fibonacci(i - 1) + fibonacci(i - 2);
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 }
