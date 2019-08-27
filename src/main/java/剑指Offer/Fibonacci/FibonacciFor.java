a64ff71822feef6185c5bbe06582fed4e6d9c6f7
yuteng <soft_yt@163.com>
2019-08-27 15:46:16 +0800

    Fibonacci 循环优化

--- src/main/java/剑指Offer/Fibonacci/FibonacciFor.java
+++ src/main/java/剑指Offer/Fibonacci/FibonacciFor.java
@@ -4,7 +4,7 @@ package 剑指Offer.Fibonacci;
  * 考虑到第 i 项只与第 i-1 和第 i-2 项有关，因此只需要存储前两项的值就能求解第 i 项，从而将空间复杂度由 O(N) 降低为 O(1)。
  * 递归效率低，使用循环。从前向后
  */
-public class Solution1 {
+public class FibonacciFor {
     public static int Fibonacci(int n) {
 
         if (n <= 1) {
@@ -22,7 +22,7 @@ public class Solution1 {
     }
 
     public static void main(String[] args) {
-        int fibonacci = Solution1.Fibonacci(5);
+        int fibonacci = FibonacciFor.Fibonacci(11);
         System.out.println(fibonacci);
     }
 }
