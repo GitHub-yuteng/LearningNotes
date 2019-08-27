73892565ca699efa9ac2355dd71c62a3fd639740
yuteng <soft_yt@163.com>
2019-08-27 15:19:12 +0800

    输入一个链表，从尾到头打印链表每个节点的值;

--- src/main/java/剑指Offer/printListFromTailToHead/Solution.java
+++ src/main/java/剑指Offer/printListFromTailToHead/Solution.java
@@ -1,4 +1,4 @@
-package 剑指Offer.Interview_6;
+package 剑指Offer.printListFromTailToHead;
 
 import java.util.ArrayList;
 import java.util.Stack;
@@ -12,31 +12,36 @@ class ListNode {
     }
 }
 
+/**
+ * 输入一个链表，从尾到头打印链表每个节点的值;
+ */
 public class Solution {
 
     public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> list = new ArrayList<>();
         Stack<Integer> stack = new Stack<>();
         while (listNode != null) {
             stack.add(listNode.val);
             listNode = listNode.next;
         }
-        ArrayList<Integer> ret = new ArrayList<>();
-        while (!stack.isEmpty())
-            ret.add(stack.pop());
-        return ret;
+
+        while (!stack.isEmpty()){
+            list.add(stack.pop());
+        }
+        return list;
     }
 
     public static void main(String[] args) {
         ListNode listNode1 = new ListNode(1);
         ListNode listNode2 = new ListNode(2);
         ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
 
         listNode1.next = listNode2;
         listNode2.next = listNode3;
+        listNode3.next = listNode4;
 
         ArrayList<Integer> integers = (ArrayList<Integer>) printListFromTailToHead(listNode1);
         System.out.println(integers);
     }
 }
-
-
