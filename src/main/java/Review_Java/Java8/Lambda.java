59f78d24936e83711604632af34f05bc49daeee2
yuteng <yuteng@wahaha.com.cn>
2020-08-21 09:42:47 +0800

    Lambda filter map reduce

--- src/main/java/Review_Java/Java8/Lambda.java
+++ src/main/java/Review_Java/Java8/Lambda.java
@@ -2,7 +2,12 @@ package Review_Java.Java8;
 
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * @author Yu
@@ -10,10 +15,89 @@ import java.util.function.Supplier;
 public class Lambda {
     public static void main(String[] args) {
 
-        User user = new User(1,"a",true);
+        User user = new User(1, "a", true, 100);
 
         Supplier<String> sup2 = user::getName;
         System.out.println(sup2.get());
+
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            int nextInt = random.nextInt(100);
+            list.add(nextInt);
+        }
+
+        list.forEach(item -> System.out.print(item + " "));
+        System.out.println();
+        List<Integer> collect = list.stream().filter(item -> item > 20).map(item -> item * 2).collect(Collectors.toList());
+        System.out.println(collect);
+
+        System.out.println(collect.stream().reduce(10000, (pre, item) -> item + pre));
+
+        System.out.println("=================");
+
+        List<User> userList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            User u = new User();
+            u.setScore(random.nextInt(50) + 50);
+            userList.add(u);
+        }
+
+        userList.stream().forEach(System.out::println);
+
+
+        System.out.println(userList.stream().filter(item -> item.getScore() > 60)
+                .map(item -> item.getScore()).reduce(0, (pre, item) -> pre + item));
+
+        List<User> users = userList.stream().filter(item -> item.getScore() > 60)
+                .map(item -> {
+                    item.setScore(item.getScore() * 2);
+                    return item;
+                }).collect(Collectors.toList());
+
+        System.out.println(users);
+
+        System.out.println("========FlatMap========");
+        List<String> teamIndia = Arrays.asList("Virat", "Dhoni", "Jadeja");
+        List<String> teamAustralia = Arrays.asList("Warner", "Watson", "Smith");
+        List<String> teamEngland = Arrays.asList("Alex", "Bell", "Broad");
+        List<String> teamNewZeland = Arrays.asList("Kane", "Nathan", "Vettori");
+        List<String> teamSouthAfrica = Arrays.asList("AB", "Amla", "Faf");
+        List<String> teamWestIndies = Arrays.asList("Sammy", "Gayle", "Narine");
+        List<String> teamSriLanka = Arrays.asList("Mahela", "Sanga", "Dilshan");
+        List<String> teamPakistan = Arrays.asList("Misbah", "Afridi", "Shehzad");
+
+        List<List<String>> playersInWorldCup2016 = new ArrayList<>();
+        playersInWorldCup2016.add(teamIndia);
+        playersInWorldCup2016.add(teamAustralia);
+        playersInWorldCup2016.add(teamEngland);
+        playersInWorldCup2016.add(teamNewZeland);
+        playersInWorldCup2016.add(teamSouthAfrica);
+        playersInWorldCup2016.add(teamWestIndies);
+        playersInWorldCup2016.add(teamSriLanka);
+        playersInWorldCup2016.add(teamPakistan);
+
+        // Let's print all players before Java 8
+        List<String> listOfAllPlayers = new ArrayList<>();
+
+        for(List<String> team : playersInWorldCup2016){
+            for(String name : team){
+                listOfAllPlayers.add(name);
+            }
+        }
+
+        System.out.println("Players playing in world cup 2016");
+        System.out.println(listOfAllPlayers);
+
+        // Now let's do this in Java 8 using FlatMap  将多个List 转变为 一个List
+        List<String> flatMapList = playersInWorldCup2016.stream()
+                .flatMap(pList -> pList.stream())
+                .collect(Collectors.toList());
+
+        System.out.println("List of all Players using Java 8");
+        System.out.println(flatMapList);
     }
 }
 
@@ -26,4 +110,5 @@ class User {
     private int id;
     private String name;
     private boolean flag;
+    private Integer score;
 }
--- src/main/java/ZTest/CollectionOfNull/List_Set_Map.java
+++ src/main/java/ZTest/CollectionOfNull/List_Set_Map.java
@@ -50,10 +50,10 @@ public class List_Set_Map {
     }
 
     @Test
-    public void testHashSet(){
+    public void testHashSet() {
         HashSet<String> set = new HashSet<>();
         set.add(null);
-        Assert.assertEquals(1,set.size()); //OK size = 1
+        Assert.assertEquals(1, set.size()); //OK size = 1
 
 //        set.add(null);
 //        Assert.assertEquals(2,set.size()); //Error size = 1
@@ -61,31 +61,31 @@ public class List_Set_Map {
     }
 
     @Test
-    public void testTreeSet(){
+    public void testTreeSet() {
         TreeSet<String> set = new TreeSet<>();
         set.add(null); //Error NullPointException
     }
 
     @Test
-    public void VectorTest(){
+    public void VectorTest() {
         Vector box = new Vector();
         box.add(null);
         box.add(null);
-        Assert.assertEquals(2,box.size()); //ok
+        Assert.assertEquals(2, box.size()); //ok
     }
 
     @Test
-    public void HashTableTest(){
+    public void HashTableTest() {
         Hashtable table = new Hashtable();
-        table.put(new Object(),null); //Exception
+        table.put(new Object(), null); //Exception
 //        table.put(null,new Object()); //Exception
 //        table.put(null,null); //Exception
-        Assert.assertEquals(1,table.size());//NullPointerException
+        Assert.assertEquals(1, table.size());//NullPointerException
     }
 
     @Test
-    public void ConcurrentHashMapTest(){
-        ConcurrentHashMap<String,String> concurrentHashMap = new ConcurrentHashMap<>();
-        concurrentHashMap.put(null,"");
+    public void ConcurrentHashMapTest() {
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+        concurrentHashMap.put(null, "");
     }
 }
